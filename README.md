dd19e187dbd66e10de7d3a763420b8c9a79f61d2
Emilio <editrocchio@gmail.com>
2018-02-28 23:28:15 -0800

    added method to finalize reporting onto website

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is a project to help automate a task of reporting information to a governme
 can be 300-600 rows of data in an Excel sheet that need to be manually entered. I'm using openpyxl to pull the required 
 information from Excel and selenium to enter it into the website.
 
+Program is complete and I will begin testing on real records in the near future.
--- wd_automator.py
+++ wd_automator.py
@@ -1,8 +1,7 @@
 import openpyxl
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 import datetime
-import traceback
-# traceback.print_exc()
 import time
 import os
 
@@ -32,58 +31,44 @@ class ExcelParser(object):
         course_end_cell = str(course_end_cell)[:10]
         return datetime.datetime.strptime(course_end_cell, "%Y-%m-%d").year
 
-    def parse_type(self, row):
-        type_cell = sheet["AB" + str(row)]
-        if type_cell.value[0:2].upper() == "WD" or type_cell.value[0:2].upper() == "UC" or type_cell.value[
-                                                                                           0:2].upper() == "EC":
-            return type_cell.value[0:2]
-        else:
-            print("Error, couldn't parse type")
-    # TODO: The following don't really work if the input isn't perfect, take out the slicing and make it so you put one option per cell
+    def parse_wd_type(self, row):
+        wd_type_cell = sheet["AB" + str(row)]
+        if wd_type_cell.value.upper() == "WD" or wd_type_cell.value.upper() == "UC" or wd_type_cell.value.upper() == \
+                "EC":
+            return wd_type_cell.value
+
     # 1: Student did not successfully complete all funded courses. If student withdrew, provide end date.
     # 2: Student withdrew at end of first term (provide end date).
     # 3: Student successfully completed studies early (provide end date).
     # 4: Student increased course load to full-time (provide end date).
     def parse_reason(self, row):
-        reason_cell = sheet["AB" + str(row)]
-        if reason_cell.value[4] == "1" or reason_cell.value[4] == "2" or reason_cell.value[4] == "3" or \
-                reason_cell.value[4] == "4":
-            return reason_cell.value[4]
-        else:
-            print("Error, couldn't parse reason")
+        reason_cell = sheet["AC" + str(row)]
+        if reason_cell.value == 1 or reason_cell.value == 2 or reason_cell.value == 3 or reason_cell.value == 4:
+            return reason_cell.value
 
     def parse_nonpunitive(self, row):
-        nonpunitive_cell = sheet["AB" + str(row)]
-        if nonpunitive_cell.value[7] == "Y".upper():
+        nonpunitive_cell = sheet["AD" + str(row)]
+        if nonpunitive_cell.value is not None and nonpunitive_cell.value.upper() == "Y":
             return True
         else:
             return False
 
     def parse_date(self, row):
-        date_cell = sheet["AB" + str(row)]
+        date_cell = sheet["AE" + str(row)]
 
-        if sheet["AB" + str(row)].value[7] == "Y".upper():
-            if date_cell.value[10]:
-                return date_cell.value[10:]
-            else:
-                print("No date specified")
-                return
-        else:
-            if date_cell.value[7]:
-                return date_cell.value[7:]
-            else:
-                return
+        if date_cell.value is not None:
+            return date_cell.value
 
 
 class StudentInfo(object):
 
-    def __init__(self, last_name, sin, end_month, end_year, type, reason, nonpunitive=False,
+    def __init__(self, last_name, sin, end_month, end_year, wd_type, reason, nonpunitive=False,
                  last_attended_date=None):
         self.last_name = last_name
         self.sin = sin
         self.end_month = end_month
         self.end_year = end_year
-        self.type = type
+        self.wd_type = wd_type
         self.reason = reason
 
         if nonpunitive is False:
@@ -108,8 +93,8 @@ class StudentInfo(object):
     def get_end_year(self):
         return self.end_year
 
-    def get_type(self):
-        return self.type
+    def get_wd_type(self):
+        return self.wd_type
 
     def get_reason(self):
         return self.reason
@@ -121,7 +106,7 @@ class StudentInfo(object):
         return self.last_attended_date
 
     def print_student_info(self):
-        print(self.last_name, self.sin, self.end_month, self.end_year, self.type, self.reason, self.nonpunitive,
+        print(self.last_name, self.sin, self.end_month, self.end_year, self.wd_type, self.reason, self.nonpunitive,
               self.last_attended_date)
 
     def write_error(self, item, excel_row):
@@ -176,12 +161,52 @@ class WebsiteNavigator(object):
                 return
 
             # If it gets this far then all tests passed, move on to withdrawal reporting page
-            driver.find_element_by_xpath('//*[@title="Application Number"]').click()
-            driver.find_element_by_xpath('//ul[@class="sailTabs"]/li[6]//div[@class="greyRt"]').click()
+            wn.enter_withdrawal_info(current_row)
 
+        # Throws exception if SIN didn't work, clear the box and write error
         except:
             driver.find_element_by_id("sin").clear()
             s.write_error("SIN", current_row)
+
+    def enter_withdrawal_info(self, row):
+        driver.find_element_by_xpath('//*[@title="Application Number"]').click()
+        driver.find_element_by_xpath('//ul[@class="sailTabs"]/li[6]//div[@class="greyRt"]').click()
+
+        if s.get_wd_type().upper() == "WD":
+            driver.find_element_by_id("withdrawalForm_withdrawalBean_withdrawalTypeId790").click()
+        elif s.get_wd_type().upper() == "EC":
+            driver.find_element_by_id("withdrawalForm_withdrawalBean_withdrawalTypeId791").click()
+        elif s.get_wd_type().upper() == "UC":
+            driver.find_element_by_id("withdrawalForm_withdrawalBean_withdrawalTypeId792").click()
+        else:
+            s.write_error("Type of withdrawal", row)
+
+        if s.get_reason() == 1:
+            driver.find_element_by_id("withdrawalForm_withdrawalBean_withdrawalReasonId793").click()
+        elif s.get_reason() == 2:
+            driver.find_element_by_id("withdrawalForm_withdrawalBean_withdrawalReasonId794").click()
+        elif s.get_reason() == 3:
+            driver.find_element_by_id("withdrawalForm_withdrawalBean_withdrawalReasonId795").click()
+        elif s.get_reason() == 4:
+            driver.find_element_by_id("withdrawalForm_withdrawalBean_withdrawalReasonId796").click()
+        else:
+            s.write_error("Reason for withdrawal", row)
+
+        # Remaining entries are optional, so no error reporting if they don't exist
+        if s.get_nonpunitive():
+            Select(driver.find_element_by_id("withdrawal_nonPunitive")).select_by_visible_text("Yes")
+        elif not s.get_nonpunitive():
+            Select(driver.find_element_by_id("withdrawal_nonPunitive")).select_by_visible_text("No")
+        else:
+            pass
+
+        if s.get_last_attended_date() is not None:
+            driver.find_element_by_id("withdrawal_dateLastAttended").send_keys(s.get_last_attended_date())
+        else:
+            pass
+
+        #TODO: Uncomment this when testing on real records. Also will need to go back to SIN entry page for next record
+        # driver.find_element_by_id("saveButton").click()
 
     def get_name(self):
         return input("Enter username: ")
@@ -193,9 +218,10 @@ class WebsiteNavigator(object):
 p = ExcelParser()
 wn = WebsiteNavigator()
 
-wn.open_sail("xxx", wn.get_name(), wn.get_pass())
+wn.open_sail("https://www.sail.aved.gov.bc.ca/sail/search/applicationSearch.action", wn.get_name(), wn.get_pass())
 print("Logging in....")
 
+# Look for the username box and if it throws an exception then the credentials were correct
 while True:
     try:
         if driver.find_element_by_id("user"):
@@ -219,7 +245,7 @@ while True:
 
 for r in range(2, sheet.max_row + 1):
     s = StudentInfo(p.parse_last_name(r), p.parse_sin(r), p.parse_end_month(r),
-                    p.parse_end_year(r), p.parse_type(r), p.parse_reason(r), p.parse_nonpunitive(r), p.parse_date(r))
+                    p.parse_end_year(r), p.parse_wd_type(r), p.parse_reason(r), p.parse_nonpunitive(r), p.parse_date(r))
 
     wn.enter_sin(s.get_sin())
     wn.check_identity(s.get_last_name(), str(s.get_end_year()), str(s.get_end_month()), r)
